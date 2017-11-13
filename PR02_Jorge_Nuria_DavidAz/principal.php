275491412c9875d27943e533a38c9b0a51170111
David <davidaznar366@gmail.com>
2017-11-13 15:56:26 +0100

    Estilos

--- PR02_Jorge_Nuria_DavidAz/principal.php
+++ PR02_Jorge_Nuria_DavidAz/principal.php
@@ -2,20 +2,21 @@
 <html>
 <head>
 	<title></title>
+	<link rel="stylesheet" type="text/css" src="css/style.css">
 </head>
 <body>
-
-	<div id="cabecera"  class="page-header">
+<!-- 
+	<div id="cabecera">
 		<h1>PR02</h1>
-	</div>
+	</div> -->
 	<div id="contenedor_izq">
-		
+		<p>fsdcdscs</p>
 	</div>
 	<div id="contenedor_derch">
-		
+		<p>fsdcdscs</p>
 	</div>
 	<div id="contenedor_central">
-		
+		<p>fsdcdscs</p>
 	</div>
 
 </body>
