0c1456358f36e184cb77b0800de43aecb996ec3c
atlanta948 <atlanta9489@gmail.com>
2017-11-13 15:22:21 +0100

    Actualización BD

--- BD/bd_empresa.sql
+++ BD/bd_empresa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-11-2017 a las 22:01:22
+-- Tiempo de generación: 13-11-2017 a las 15:21:19
 -- Versión del servidor: 10.1.26-MariaDB
 -- Versión de PHP: 7.1.9
 
@@ -43,7 +43,7 @@ CREATE TABLE `tbl_incidencia` (
 
 CREATE TABLE `tbl_recurso` (
   `idRecurso` int(4) NOT NULL,
-  `nombreRecursos` varchar(15) DEFAULT NULL,
+  `nombreRecursos` varchar(35) DEFAULT NULL,
   `tipoRecursos` enum('Sala','Material') DEFAULT NULL,
   `Ocupado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -53,10 +53,21 @@ CREATE TABLE `tbl_recurso` (
 --
 
 INSERT INTO `tbl_recurso` (`idRecurso`, `nombreRecursos`, `tipoRecursos`, `Ocupado`) VALUES
-(1, 'Aula teoría ', 'Sala', 0),
-(2, 'Aula teoría con', 'Sala', 0),
-(3, 'movil', 'Material', 0),
-(4, 'portatil', 'Material', 0);
+(1, 'Aula teoría con projector 01', 'Sala', 0),
+(2, 'Aula teoría con projector 02', 'Sala', 0),
+(3, 'Aula teoría sin projector', 'Sala', 0),
+(4, 'Aula informática 01', 'Sala', 0),
+(5, 'Aula informática 02', 'Sala', 0),
+(6, 'Despacho para entrevistas 01', 'Sala', 0),
+(7, 'Despacho para entrevistas 02', 'Sala', 0),
+(8, 'Sala de reuniones', 'Sala', 0),
+(9, 'Proyector portátil', 'Material', 0),
+(10, 'Carro de portátiles', 'Material', 0),
+(11, 'Portátil', 'Material', 0),
+(12, 'Portátil ', 'Material', 0),
+(13, 'Portátil', 'Material', 0),
+(14, 'Móvil', 'Material', 0),
+(15, 'Móvil', 'Material', 0);
 
 -- --------------------------------------------------------
 
@@ -85,16 +96,25 @@ CREATE TABLE `tbl_usuario` (
   `dniUsuario` varchar(9) DEFAULT NULL,
   `mailUsuario` varchar(35) DEFAULT NULL,
   `telefonoUsuario` int(9) DEFAULT NULL,
-  `direccionUsuario` varchar(60) DEFAULT NULL
+  `direccionUsuario` varchar(60) DEFAULT NULL,
+  `passwordUsuario` varchar(12) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `tbl_usuario`
 --
 
-INSERT INTO `tbl_usuario` (`idUsuario`, `nombreUsuario`, `apellidoUsuario`, `dniUsuario`, `mailUsuario`, `telefonoUsuario`, `direccionUsuario`) VALUES
-(1, 'Juan', 'Alvarez', '45246798L', 'juanalvarez@gmail.com', 612346789, 'juanalvarez@gmail.com'),
-(2, 'Carlos', 'Pedro', '56934567F', 'carlospedro@gmail.com', 625678946, 'C/ Balmes nª 2 1º4');
+INSERT INTO `tbl_usuario` (`idUsuario`, `nombreUsuario`, `apellidoUsuario`, `dniUsuario`, `mailUsuario`, `telefonoUsuario`, `direccionUsuario`, `passwordUsuario`) VALUES
+(1, 'Juan', 'Alvarez', '45246798L', 'juanalvarez@gmail.com', 612346789, 'juanalvarez@gmail.com', '12345'),
+(2, 'Carlos', 'Pedro', '56934567F', 'carlospedro@gmail.com', 625678946, 'C/ Balmes nº 2 1º4', '5678'),
+(3, 'Helena', 'Perez', '67434678N', 'helenaperez@gmail.com', 634689457, 'C/ Gerona nº 4 2º1', '6789'),
+(4, 'Jordi', 'Baró', '56746745M', 'jordibaro@gmail.com', 634683456, 'C/ Amalia nº 6  2º4', '23456'),
+(5, 'Victor', 'Cabrera', '54237854R', 'victorcabrera@gmail.com', 623578546, 'victorcabrera@gmail.com', '3456'),
+(6, 'Xavi', 'Campo', '26745778F', 'xavicampo@gmail.com', 623577878, 'C/ Ribes nº 9 3º1', '67890'),
+(7, 'Ricardo', 'Jaume', '45736896L', 'ricardojaume@gmail.com', 675456854, 'C/ Gava nº 4 1º2', '7890'),
+(8, 'Fran', 'López', '86456567G', 'franlopez@gmail.com', 633478789, 'C/ Miro nº 2 4º2', '234567'),
+(9, 'Sergi', 'Mateo', '34656546D', 'sergimateo@gmail.com', 635457567, 'C/ Tarragona', '567890'),
+(10, 'Alba', 'Vilanova', '63468246K', 'albavilanova@gmail.com', 634687804, 'C/ Olesa nº 3 2º2', '345678');
 
 --
 -- Índices para tablas volcadas
@@ -141,19 +161,19 @@ ALTER TABLE `tbl_incidencia`
 -- AUTO_INCREMENT de la tabla `tbl_recurso`
 --
 ALTER TABLE `tbl_recurso`
-  MODIFY `idRecurso` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `idRecurso` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT de la tabla `tbl_reserva`
 --
 ALTER TABLE `tbl_reserva`
-  MODIFY `idReserva` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `idReserva` int(4) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `tbl_usuario`
 --
 ALTER TABLE `tbl_usuario`
-  MODIFY `idUsuario` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idUsuario` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Restricciones para tablas volcadas
