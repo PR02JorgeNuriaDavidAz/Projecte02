9a9f3826258cf37cf89d7d86ebaa503b0dd5c409
NuriaGA <nuriagomezArn@gmail.com>
2017-11-16 20:40:25 +0100

    Subir reserva, estilos, filtros

---
+++ Nuria/Proyecto_Reserva/prueba2.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<style>
+a {
+    color: hotpink;
+}
+</style>
+	<title></title>
+</head>
+<body>
+<a href="principal.html" target="_blank">Inicio</a>	
+
+</body>
+</html>
+
+
+
+
+<?php
+
+
+
+
+
+
+?>
